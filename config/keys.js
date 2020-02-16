d3bcd0110333403e215a5757f0ea09fcdd606d48
Prathibha R <prathi2kgrs@gmail.com>
2020-02-16 16:25:58 +0530

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://prathi2kgrs:'+ encodeURIComponent('mathispower') + '@cluster0-ypbn0.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
